3ee8cb08149027b0521d426650adf71ed001f173
José Tomás Navarro Carrión <jt.navarrocarrion@gmail.com>
2018-07-13 09:47:19 +0200

    Rewritten initdb-postgis.sh
    Removed EXPORT.
    Removed command expansion using braces: "${psql[@]}". Use `psql` string literal instead.
    Removed tab chars. Use whitespace chars instead.

--- lib/postgis/2.4.4/initdb-postgis.sh
+++ lib/postgis/2.4.4/initdb-postgis.sh
@@ -2,21 +2,18 @@
 
 set -e
 
-# Perform all actions as $POSTGRES_USER
-export PGUSER="$POSTGRES_USER"
-
 # Create the 'template_postgis' template db
-"${psql[@]}" <<- 'EOSQL'
-CREATE DATABASE template_postgis;
-UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE DATABASE template_postgis;
+  UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
 EOSQL
 
 # Load PostGIS into both template_database and $POSTGRES_DB
 for DB in template_postgis "$POSTGRES_DB"; do
-	echo "Loading PostGIS and plpython3u extensions into $DB"
-	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
-		CREATE EXTENSION IF NOT EXISTS plpython3u;
-		CREATE EXTENSION IF NOT EXISTS postgis;
-		CREATE EXTENSION IF NOT EXISTS postgis_topology;
+  echo "Loading PostGIS and plpython3u extensions into $DB"
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB"<<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS plpython3u;
+    CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS postgis_topology;
 EOSQL
 done
